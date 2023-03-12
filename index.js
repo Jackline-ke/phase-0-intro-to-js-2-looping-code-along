c65db268251ea2fc3c34579afa18bcd5e72c749f
Jackline-ke <antoninahodhiambo@gmail.com>
2023-03-12 17:04:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName){
+    const thankYouMessages = [];
+    for(let i = 0; i < names.length; i++){
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return thankYouMessages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], `surprise`);
+
+
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number)
+        number--;
+
+    }
+}
+countDown(10);
